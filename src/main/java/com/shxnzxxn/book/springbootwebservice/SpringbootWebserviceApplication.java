e86d75bd9f586142d7cd395f53551591f8ad0792
skj4244 <skj4244@naver.com>
2024-01-16 01:24:40 +0900

    init

---
+++ src/main/java/com/shxnzxxn/book/springbootwebservice/SpringbootWebserviceApplication.java
@@ -0,0 +1,15 @@
+package com.shxnzxxn.book.springbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebserviceApplication.class, args);
+		System.out.println("hwllo");
+
+	}
+
+}
