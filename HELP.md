58dec8aa22301cda650fcd4daea493b2bcb73194
skj4244 <skj4244@naver.com>
2024-01-23 11:58:01 +0900

    1/23

--- HELP.md
+++ HELP.md
@@ -1,7 +1,7 @@
 # Read Me First
 The following was discovered as part of building this project:
 
-* The original package name 'com.shxnzxxn.book.springboot-webservice' is invalid and this project uses 'com.shxnzxxn.book.springbootwebservice' instead.
+* The original package name 'com.shxnzxxn.book.springboot-webservice' is invalid and this project uses 'com.shxnzxxn.book.springboot' instead.
 
 # Getting Started
 
---
+++ src/test/java/com/shxnzxxn/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.shxnzxxn.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩() throws Exception {
+        // when
+        String body = restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+
+    }
+
+}
